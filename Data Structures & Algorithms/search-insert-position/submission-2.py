72161c44b3dce43e6eab3df1ae13889c64ad645b
Vertu <61234742+Vertu5@users.noreply.github.com>
2026-07-12 19:43:33 +0200

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+            
+            if target > nums[middle]:
+                left = middle + 1
+            else:
+                right = middle - 1
+                
+        # Si on sort de la boucle, c'est qu'on n'a rien trouvé.
+        # 'left' pointe naturellement sur le futur index d'insertion !
+        return left
